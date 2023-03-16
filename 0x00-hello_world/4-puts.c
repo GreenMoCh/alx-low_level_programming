68cc3e704d75d27e6bff0418a0df5a5954778339
GreenMoCh <moadchabaa0@gmail.com>
2023-03-16 12:31:14 -0700

    DONE !!

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
- * main - Entry point
- *
- * Return: Alwyas 0 (Success)
- */
+* main - Entry point
+*
+* Return: Alwyas 0 (Success)
+*/
 int main(void) {
     puts("\"Programming is like building a multilingual puzzle");
-    return 0;
+    return (0);
 }
