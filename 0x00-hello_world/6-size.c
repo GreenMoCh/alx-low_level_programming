0c6c034f4b33b2970efb97e80871a68d8e704b99
GreenMoCh <moadchabaa0@gmail.com>
2023-03-16 12:27:25 -0700

    C

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,8 +2,8 @@
 /**
  * main - Entry point
  *
- * Return: Alwyas 0 (Success)
- */
+ * Return: Always 0 (Success)
+*/
 int main(void) {
     printf("Size of a char: %d byte(s)\n", sizeof(char));
     printf("Size of an int: %d byte(s)\n", sizeof(int));
